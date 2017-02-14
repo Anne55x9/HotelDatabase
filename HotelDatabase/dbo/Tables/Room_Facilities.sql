4dc75f87a4baa54e15f3cf826df6de7962720bc1
anne55x9 <anne55x9@edu.easj.dk>
2017-02-14 15:21:05 +0100

    HotelDataBase med tabeller. Sidst er en room facilitetes tabel indført.

---
+++ HotelDatabase/dbo/Tables/Room_Facilities.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Room_Facilities]
+(
+	[Room_no] INT NOT NULL PRIMARY KEY, 
+    [Facility_no] INT NOT NULL, 
+    CONSTRAINT [FK_Room_Facilities_Facilities] FOREIGN KEY ([Facility_no]) REFERENCES [Facilities](Facility_no), 
+     
+     
+   
+)
